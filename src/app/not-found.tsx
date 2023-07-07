da0e466cdb997f2ab71272647256480aab733fa6
butterbeetle <butterbeetle@naver.com>
2023-07-07 19:50:52 +0900

    STUDY : 여러가지
    1.라우팅
    2.layout

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <p>View all posts</p>
+    </div>
+  );
+}
---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PantsPage({ params }: Props) {
+  return <h1>{params.slug} page</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({ slug: product }));
+}
