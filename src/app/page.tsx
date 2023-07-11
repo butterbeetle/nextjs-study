3a5aca74f2eef26969bd3ad846fd24ea9f035e8e
butterbeetle <butterbeetle@naver.com>
2023-07-11 15:22:26 +0900

    STUDY : SSG 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Counter from "@/components/Counter";
 import os from "os"; // 노드 APIs
 
 export default function Home() {
-  console.log("안뇽");
+  console.log("안녕 - 서버");
   console.log(os.hostname());
 
   return (
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,20 +1,30 @@
+import NotFoundPage from "@/app/not-found";
+import { getProduct, getProducts } from "@/service/products";
+
 type Props = {
   params: {
     slug: string;
   };
 };
 
-export function generateMetadata({ params }: Props) {
+export function generateMetadata({ params: { slug } }: Props) {
   return {
-    title: `제품의 이름: ${params.slug}`,
+    title: `제품의 이름: ${slug}`,
   };
 }
 
-export default function PantsPage({ params }: Props) {
-  return <h1>{params.slug} page</h1>;
+export default async function ProductsPage({ params: { slug } }: Props) {
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌.
+  const product = await getProduct(slug);
+
+  if (!product) {
+    NotFoundPage();
+  }
+  return <h1>{product?.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  const products = ["pants", "skirt"];
-  return products.map((product) => ({ slug: product }));
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임. (SSG)
+  const products = await getProducts();
+  return products.map((product) => ({ slug: product.id }));
 }
