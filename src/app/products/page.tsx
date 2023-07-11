c22f1b86a67ef1dbcf4a5590f8a12d7727590a4d
butterbeetle <butterbeetle@naver.com>
2023-07-11 19:43:38 +0900

    STUDY : Loading, Error UI

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,6 +5,7 @@ import MeowArticle from "@/components/MeowArticle";
 
 export default async function ProductsPage() {
   const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지</h1>
