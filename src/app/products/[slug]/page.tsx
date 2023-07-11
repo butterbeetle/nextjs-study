42e8baff6dd2cb600eb85e9cb3285a2482a34117
butterbeetle <butterbeetle@naver.com>
2023-07-11 15:27:57 +0900

    STUDY : ISR 렌더링
    revalidate 로 몇초에 한번 할껀지 정할 수 있다.

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import NotFoundPage from "@/app/not-found";
 import { getProduct, getProducts } from "@/service/products";
 
+export const revalidate = 3;
+
 type Props = {
   params: {
     slug: string;
@@ -13,6 +15,12 @@ export function generateMetadata({ params: { slug } }: Props) {
   };
 }
 
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임. (SSG)
+  const products = await getProducts();
+  return products.map((product) => ({ slug: product.id }));
+}
+
 export default async function ProductsPage({ params: { slug } }: Props) {
   // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌.
   const product = await getProduct(slug);
@@ -21,10 +29,4 @@ export default async function ProductsPage({ params: { slug } }: Props) {
     NotFoundPage();
   }
   return <h1>{product?.name} 제품 설명 페이지</h1>;
-}
-
-export async function generateStaticParams() {
-  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임. (SSG)
-  const products = await getProducts();
-  return products.map((product) => ({ slug: product.id }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,9 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
+export const revalidate = 3;
+
 export default async function ProductsPage() {
-  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와 그걸 보여줌.
   const products = await getProducts();
   return (
     <>
